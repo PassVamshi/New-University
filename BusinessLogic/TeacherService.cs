5d1352111c2e1cb3c731066c32fe752955de001d
Mahesh Patnam <96472018+PassVamshi@users.noreply.github.com>
2022-11-19 17:35:31 +0530

    Add files via upload

---
+++ BusinessLogic/TeacherService.cs
@@ -0,0 +1,43 @@
+﻿using Database;
+using EntitiesAndModels.Entities;
+
+namespace BusinessLogic
+{
+    public interface ITeacherService
+    {
+        Teacher AddTeacher(Teacher teacher);
+        IEnumerable<Teacher> GetTeachersWithDepartment(int id);
+        Teacher GetTeacherWithId(int id);
+    }
+    public class TeacherService : ITeacherService
+    {
+        private readonly UniversityContext _universityContext;
+        public TeacherService(UniversityContext universityContext)
+        {
+            _universityContext = universityContext;
+        }
+        public Teacher AddTeacher(Teacher teacher)
+        {
+            try
+            {
+                _universityContext.Teachers.Add(teacher);
+                _universityContext.SaveChanges();
+                return teacher;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+        public IEnumerable<Teacher> GetTeachersWithDepartment(int id)
+        {
+           var teachers= _universityContext.Teachers.Where(x => x.Department == id);
+            return teachers;
+        }
+        public Teacher GetTeacherWithId(int id)
+        {
+            return _universityContext.Teachers.Single(x => x.Id == id);
+        }
+    }
+}
